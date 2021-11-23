14bb2e9dc96df0f8856b552ebdbe4daabaa312df
JakubGawron1 <jakubgawron6@gmail.com>
2021-11-23 17:06:39 +0100

    Add deploy function to repo

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+git config --global user.name "CircleCI"
+git config --global user.email "<>"
+git pull
+git add -A
+git commit -m "[ci skip] Deploy nightly build"
+
+git push $(git config --get remote.origin.url) master:master
